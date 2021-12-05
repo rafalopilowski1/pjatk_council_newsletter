821b56f3d2aaaea128592f44d9bd677b08ee0b18
Krystian Postek <krystian@postek.eu>
2021-12-05 23:21:32 +0100

    Replace user-agent with something modern

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 async fn get_data(feed_url: &str) -> Result<Feed, Box<dyn Error>> {
     let client = reqwest::Client::builder()
-        .user_agent("Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
         .build()?;
     let request = client.get(feed_url);
     let response = request.send().await?;
