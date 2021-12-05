a5accbaa1c1b210f61bfb27c0c231f9b2ca3fb01
Rafał Opiłowski <rafalopilowski1@outlook.com>
2021-12-05 23:11:45 +0100

    Version 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use atom_syndication::*;
 use ferrishook::*;
+use regex::Regex;
+
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://samorzad.pja.edu.pl";
     let feed: Feed = get_data("https://samorzad.pja.edu.pl/feed/atom").await?;
-    send_webhook(&feed).await
+    send_webhook(&feed, url).await
 }
 
 async fn get_data(feed_url: &str) -> Result<Feed, Box<dyn Error>> {
@@ -19,21 +22,45 @@ async fn get_data(feed_url: &str) -> Result<Feed, Box<dyn Error>> {
     Ok(feed)
 }
 
-async fn send_webhook(feed: &Feed) -> Result<(), Box<dyn Error>> {
+async fn send_webhook(feed: &Feed, url: &str) -> Result<(), Box<dyn Error>> {
     let secret = "https://discord.com/api/webhooks/917158640119611442/K8F4SWhJGzqM0GiH3VVy36wQlLK02VoVz2EVXTLbkZcnLG3gi_pvVuW09rXxEteL9nQs";
     Ok(webhook::new(secret, |webhook| {
-        let feed_entry = feed.entries().last().unwrap();
+        let feed_entry = feed.entries().first().unwrap();
         webhook.username("Listonosz PJATK").embed(|embed| {
-            let feed_content = feed_entry.content()?.value()?;
+            let mut feed_content_str = feed_entry.content().unwrap().value().unwrap();
+            let feed_content_dom = scraper::html::Html::parse_document(feed_content_str);
+            let mut text = String::new();
+            for textOnce in feed_content_dom.root_element().text().into_iter() {
+                text.push_str(textOnce);
+            }
+            let regex = Regex::new(r"\n+").unwrap();
+            text = regex.replace_all(&mut text, "\n\n").to_string();
+            let mut feed_content_image_url = String::from(url);
+            for image_element in feed_content_dom
+                .select(&scraper::Selector::parse("img").unwrap())
+                .take(1)
+            {
+                feed_content_image_url.push_str(image_element.value().attr("src").unwrap());
+            }
+
             embed
                 .author(|author| {
                     let feed_author = feed_entry.authors().first().unwrap();
                     author.name(feed_author.name())
-                    //.url(feed_author.uri().unwrap())
                 })
-                .color(0)
-                .description(feed_content)
-                .footer(|footer| footer.text(feed_entry.published().unwrap().to_rfc2822()))
+                .color(0x8ebda7)
+                .image(feed_content_image_url)
+                .description(text)
+                .footer(|footer| {
+                    footer.text(
+                        feed_entry
+                            .published()
+                            .unwrap()
+                            .naive_local()
+                            .format("%d-%m-%Y %H:%M:%S")
+                            .to_string(),
+                    )
+                })
                 .title(feed_entry.title())
         })
     })
